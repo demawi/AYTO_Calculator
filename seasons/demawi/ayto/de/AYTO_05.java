32f8921f6cd21f3f2f617454e20d3997587c6549
demawi <demawi@gmail.com>
2023-11-18 23:52:14 +0100

    de.05 Start

---
+++ seasons/demawi/ayto/de/AYTO_05.java
@@ -0,0 +1,49 @@
+package demawi.ayto.de;
+
+import demawi.ayto.modell.Frau;
+import demawi.ayto.modell.Mann;
+import demawi.ayto.modell.StaffelData;
+import demawi.ayto.permutation.AYTO_Permutator;
+import demawi.ayto.print.DefaultMatchPrinter;
+
+import static demawi.ayto.modell.AYTO_Pair.pair;
+
+public class AYTO_05
+      extends StaffelData {
+
+   private final Frau Afra = frau("Afra");
+   private final Frau Edda = frau("Edda");
+   private final Frau Jana = frau("Jana");
+   private final Frau Julia = frau("Julia");
+   private final Frau Lina = frau("Lina");
+   private final Frau Lisa_Marie = frau("Lisa-Marie");
+   private final Frau Maja = frau("Maja");
+   private final Frau Melanie = frau("Melanie");
+   private final Frau Pia = frau("Pia");
+   private final Frau Shelly = frau("Shelly");
+   private final Frau Sina = frau("Sina");
+   private final Frau Tais = frau("Tais");
+
+   private final Mann Eti = mann("Eti");
+   private final Mann Gerrit = mann("Gerrit");
+   private final Mann Kevin = mann("Kevin");
+   private final Mann Martin = mann("Martin");
+   private final Mann Paddy = mann("Paddy");
+   private final Mann Paul = mann("Paul");
+   private final Mann Ryan = mann("Ryan");
+   private final Mann Sandro = mann("Sandro");
+   private final Mann Sidar = mann("Sidar");
+   private final Mann Wilson = mann("Wilson");
+
+   public AYTO_05() {
+      super(AYTO_Permutator.ZUSATZTYPE.NUR_LETZTER);
+
+      newTag().matchBox(pair(Jana, Paul), null);
+
+   }
+
+   public static void main(String[] args) {
+      new DefaultMatchPrinter().printDayResults(new AYTO_05());
+   }
+
+}
